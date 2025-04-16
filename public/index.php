390c48a1d52391c88ce7083fa6ae6d284d153088
sebasmzg <j.sebastianmzg@gmail.com>
2025-04-16 16:35:07 -0500

    Add cors

--- public/index.php
+++ public/index.php
@@ -1,4 +1,15 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Expose-Headers: Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
